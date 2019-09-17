b164f63691c3d1b99d66c4af8f417e96f6e5f0c5
Anthony <anthonyramnarain7@gmail.com>
2019-09-17 10:14:38 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/* ----------------------------------------------------------------------------
+write a simulation program
+customers come need service at a station (single server) anywhere from 0 to 5 min and anywhere from 0 to 8 min is the arrival times between customers. use your program to find the avg amount of time a customer is in the system, the variance of that time, the avg time the server is idle and the variance of this time.
+
+run for 100,000 customers leaving the system.
+
+include debugging output for the first 15 customers leaving the system of their arrival time, their service requirement clock time updates and the queues
+ */
+
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+
+    }
+}
